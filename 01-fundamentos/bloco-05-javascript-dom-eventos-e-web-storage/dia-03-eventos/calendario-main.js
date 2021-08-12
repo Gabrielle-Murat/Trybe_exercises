045b06589d0f7cc673175d19f39b12b46b7b608c
Gabrielle-Murat <belle.murat@gmail.com>
2021-08-12 12:57:44 -0300

    Adiciona o exercício 2

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/calendario-main.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/calendario-main.js
@@ -47,6 +47,16 @@ handleCalendarDayCreation();
 //2.1- Adicione a este botão a ID "btn-holiday".
 //2.2- Adicione este botão como filho/filha da tag <div> com classe "buttons-container".
 
+function createHolidayButton(string) {
+  let button = document.createElement('button');
+  let div = document.getElementsByClassName('buttons-container');
+  button.innerText = string;
+  button.id = 'btn-holiday';
+
+  div[0].appendChild(button);
+}
+
+createHolidayButton('Feriados');
 
 //3- Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday".
 //3.1- É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)".
