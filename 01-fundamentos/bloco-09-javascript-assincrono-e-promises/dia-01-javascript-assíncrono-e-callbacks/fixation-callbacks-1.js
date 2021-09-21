d50e17205972d13c1303b80d872e4cdfd046a00a
Gabrielle-Murat <belle.murat@gmail.com>
2021-09-21 13:43:13 -0300

    Adiciona os exercícios do bloco 09

---
+++ 01-fundamentos/bloco-09-javascript-assincrono-e-promises/dia-01-javascript-assíncrono-e-callbacks/fixation-callbacks-1.js
@@ -0,0 +1,29 @@
+//1 - No código abaixo você tem a função getUser , que retorna uma pessoa qualquer. Complete a função getUser de forma que ela receba uma outra função como parâmetro para que possa realizar as operações abaixo sobre a pessoa retornada.
+
+const assert = require('assert');
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = callback => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return callback(userToReturn);
+};
+
+const getFullName = (parameter) => {
+  const firstName = parameter.firstName;
+  const lastName = parameter.lastName;
+  return userFullName({ firstName, lastName });
+}
+const getNationality = (parameter) => {
+  const firstName = parameter.firstName;
+  const nationality = parameter.nationality;
+  return userNationality({ firstName, nationality });
+}
+
+assert.strictEqual(getUser(getFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+assert.strictEqual(getUser(getNationality), "Ivan is Russian"); // complete a chamada da função de getUser
