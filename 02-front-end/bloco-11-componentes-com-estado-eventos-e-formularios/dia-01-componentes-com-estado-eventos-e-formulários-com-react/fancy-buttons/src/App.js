42266ef2b65b625caa21dfbdfa2c8e23661d89b6
Gabrielle-Murat <belle.murat@gmail.com>
2021-10-08 15:27:18 -0300

    Vincula funcoes a classe com this e bind

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios/dia-01-componentes-com-estado-eventos-e-formulários-com-react/fancy-buttons/src/App.js
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios/dia-01-componentes-com-estado-eventos-e-formulários-com-react/fancy-buttons/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import './App.css';
 
-function handleFirstButton(event) {
-  return console.log(event.target.innerText);
-}
-
-function handleSecondButton(event) {
-  return console.log(event.target.innerText);
-}
-
-function handleThirdButton(event) {
-  return console.log(event.target.innerText);
-}
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleFirstButton = this.handleFirstButton.bind(this);
+    this.handleSecondButton = this.handleSecondButton.bind(this);
+    this.handleThirdButton = this.handleThirdButton.bind(this);
+  }
+
+  handleFirstButton(event) {
+    return console.log(event.target.innerText);
+  }
+
+  handleSecondButton(event) {
+    return console.log(event.target.innerText);
+  }
+
+  handleThirdButton(event) {
+    return console.log(event.target.innerText);
+  }
+
   render() {
     return (
       <section className="buttons">
-        <button className='first-button' onClick={handleFirstButton}>Olá Belle!</button>
-        <button className='second-button' onClick={handleSecondButton}>Tudo bem?</button>
-        <button className='third-button' onClick={handleThirdButton}>Parabéns pela conclusão do módulo de fundamentos web!</button>
+        <button className='first-button' onClick={this.handleFirstButton}>Olá Belle!</button>
+        <button className='second-button' onClick={this.handleSecondButton}>Tudo bem?</button>
+        <button className='third-button' onClick={this.handleThirdButton}>Parabéns pela conclusão do módulo de fundamentos web!</button>
       </section>
     )
   }
